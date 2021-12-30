0bdc7a35a18e9ffdec2dc94d523b0da7583fb0d7
Amirul Islam <amirulandalib9@gmail.com>
2021-12-30 13:29:53 +0600

    Update Greeting.js

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -41,7 +41,7 @@ export default function Greeting() {
               <div className="button-greeting-div">
                 <Button text="Contact me" href="#contact" />
                 <Button
-                  text="Join XcodersHub on Telegram"
+                  text="Join KangersHub on Telegram"
                   newTab={true}
                   href={greeting.resumeLink}
                 />
